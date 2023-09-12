89db26a0063d443222ac9a7a989040c5d4ffd237
Basit Ali <alib4111@gmail.com>
2023-09-13 04:31:00 +0500

    modal testing

--- src/components/UI/Modal/index.jsx
+++ src/components/UI/Modal/index.jsx
@@ -1,8 +1,11 @@
-import ModalCard from './ModalCard';
+// import ModalCard from './ModalCard';
 
 const Modal = ({ children }) => {
 	return (
-		<div className='flex justify-center z-50 left-0 right-0 bottom-0 top-0 w-full h-screen fixed'>
+		<div
+			id='modal'
+			className='flex justify-center z-50 left-0 right-0 bottom-0 top-0 w-full h-screen fixed'
+		>
 			<div className='bg-gray-transparent w-[500px] h-[600px] top-1/2 left-1/2 blur-0'>
 				{children}
 			</div>
--- src/pages/overview/index.jsx
+++ src/pages/overview/index.jsx
@@ -1,11 +1,52 @@
-import Modal from '../../components/UI/Modal';
+// import Modal from '../../components/UI/Modal';
+import { useState } from 'react';
+import Modal from 'react-modal';
+Modal.setAppElement('#root');
 
 const Overview = () => {
+	const [isModalOpen, setIsModalOpen] = useState(true);
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 	return (
 		<section className='text-white'>
 			{/* <Modal>
 				<h1>welcome</h1>
 			</Modal> */}
+
+			<Modal
+				isOpen={isModalOpen}
+				onRequestClose={closeModal}
+				style={{
+					overlay: {
+						backdropFilter: 'blur(7px)',
+						background: 'rgba(0, 0, 0, 0.30)', // Apply blur effect to the background
+					},
+					content: {
+						// Your modal styles here
+						width: '500px',
+						height: '500px',
+						left: '50%',
+						top: '50%',
+						transform: 'translate(-50%, -50%)',
+						borderRadius: '20px',
+						background: 'rgba(255, 255, 255, 0.30)',
+						backdropFilter: 'blur(60px)',
+						border: 'none',
+						color: 'white',
+					},
+				}}
+			>
+				{/* Your modal content goes here */}
+				<h2>Modal Title</h2>
+				<p>Modal Content</p>
+				<button onClick={closeModal}>Close Modal</button>
+			</Modal>
 			<div className='flex items-start justify-between pr-32'>
 				<div className=''>
 					<h2 className=' text-4xl font-medium'>Välkommen, Boqueria</h2>
